1d08d3ff7a8042e2b03d3d8459142504f685306f
makidelille <makidelille@bitbucket.org>
2016-10-20 19:29:00 +0200

    changes

--- js/master.js
+++ js/master.js
@@ -1,5 +1,10 @@
 
-  window.onscroll = function(){
+
+var site = "home";
+var href = window.location.href;
+
+
+window.onscroll = function(){
       if(document.body.scrollTop > 1){
         document.getElementById('mainHeader').className="min";
         document.getElementById("container").className="down";
@@ -9,14 +14,65 @@
       }
   }
 
-function scrollToTop(){
+
+window.onload = function(){
+    var lis = document.querySelectorAll('header nav li');
+    for(var i=0; i< lis.length; i++){
+      lis[i].onclick = function(event){
+        site = this.className;
+        selectInNav();
+        loadPage(site + ".html");
+      }
+    }
+
+    document.querySelector('header nav li.home').click();
+    scrollToPos(1);
+  };
+
+function scrollToPos(posY){
   var speed = 50;
-  var nextDif = document.body.scrollTop - speed;
+  var nextDif = document.body.scrollTop - speed-posY;
   if(nextDif > 0){
       window.scrollTo(0, document.body.scrollTop - nextDif/speed);
-      setTimeout(scrollToTop, 10);
+      setTimeout(function(){
+        scrollToPos(posY);
+      }, 10);
   }else{
-    window.scrollTo(0,0);
+    window.scrollTo(posY,0);
   }
 
+}
+
+
+function selectInNav(){
+  var lis = document.querySelectorAll('header nav li');
+  for(var i=0; i < lis.length; i++){
+    lis[i].removeAttribute("selected");
+  }
+  var li = document.querySelector('header nav li.' + site) || null;
+  if(li != null){
+    li.setAttribute("selected",1);
+  }
+}
+
+function loadPage(pageUrl){
+  console.log(pageUrl);
+  var update = new XMLHttpRequest();
+   update.open("GET", pageUrl, true);
+   update.onreadystatechange = function () {
+       if(update.readyState === 4 && (update.status === 200 || update.status == 0)){
+            document.getElementById("container").innerHTML = update.responseText;
+        }
+
+        document.querySelectorAll('#container a').forEach(function(a){
+          a.onclick = aOnClick;
+        });
+   }
+  update.send();
+}
+
+function aOnClick(event){
+  event.preventDefault();
+  loadPage(this.href);
+  return false;
 }
