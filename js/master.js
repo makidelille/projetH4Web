47c97abc4fc74ad8ff73305f4cc0cf29b6ab93e3
makidelille <makidelille@github.com>
2016-10-29 18:49:15 +0200

    final commit before lot1

--- js/master.js
+++ js/master.js
@@ -1,8 +1,6 @@
 
 
 var site = "home";
-var href = window.location.href;
-
 
 window.onscroll = function(){
       if(document.body.scrollTop > 1){
@@ -14,7 +12,6 @@ window.onscroll = function(){
       }
   }
 
-
 window.onload = function(){
     var lis = document.querySelectorAll('header nav li');
     for(var i=0; i< lis.length; i++){
@@ -24,8 +21,11 @@ window.onload = function(){
         loadPage(site + ".html");
       }
     }
-
-    document.querySelector('header nav li.home').click();
+    if(getCookie("last") != ""){
+      site = getCookie("last");
+    }
+    console.log(site);
+    document.querySelector('header nav li.' + site).click();
     scrollToPos(1);
   };
 
@@ -76,17 +76,26 @@ function loadPage(pageUrl){
         document.querySelectorAll('#container a').forEach(function(a){
           a.onclick = aOnClick;
         });
+        setCookie("last",site,7);
    }
   update.send();
 }
 
 function aOnClick(event){
-  event.preventDefault();
-  loadPage(this.href);
-  return false;
+  if(this.href.startsWith(window.location.origin)){
+    event.preventDefault();
+    loadPage(this.href);
+    return false;
+  }
 }
 
-function  imgLoad(evt) {
+function disconnect(){
+  clearCookie();
+  //TODO window.location.reload();
+  window.location.href="/index.html" //TODO for demo
+}
+
+function  imgLoad(evt, target) {
       var tgt = evt.target || window.event.srcElement,
           files = tgt.files;
 
@@ -95,8 +104,106 @@ function  imgLoad(evt) {
 
           var fr = new FileReader();
           fr.onload = function () {
-              document.getElementById("imgUpload").src = fr.result;
+              document.getElementById(target).src = fr.result;
           }
           fr.readAsDataURL(files[0]);
       }
 }
+
+
+function textAreaAdjust(o) {
+  o.style.height = "1px";
+  o.style.height = (o.scrollHeight)+"px";
+}
+
+//src : http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function clearCookie(){
+  document.cookie = "last=;expires=" + new Date().toUTCString();
+}
+
+
+function editProfil(){
+  document.getElementById('infos').className ="editable";
+  document.getElementById('medias').className ="editable";
+  document.querySelector('button[name="Edit"]').onclick = saveProfil;
+  document.querySelector('button[name="Edit"]').innerText = "Save";
+  document.querySelector('button[name="Edit"]').name = "Save";
+
+  document.querySelectorAll('#medias a').forEach(function(a){
+    a.onclick = function(event){
+      if(confirm("do you want to delete ?")){
+        //Supprimer
+      }else{
+
+      }
+      return false;
+    }
+  });
+  document.querySelector('#infos figure').onclick = function(){document.getElementById('profilPicInput').click()};
+}
+
+function saveProfil(){
+  document.getElementById('infos').className ="";
+  document.getElementById('medias').className ="";
+  document.querySelector('button[name="Save"]').onclick = editProfil;
+  document.querySelector('button[name="Save"]').innerText = "Edit";
+  document.querySelector('button[name="Save"]').name = "Edit";
+
+  document.querySelectorAll('#medias a').forEach(function(a){
+    a.onclick = aOnClick;
+  });
+
+  document.querySelector('#infos figure').onclick = function(){};
+
+  //send update
+}
+
+function toggleLogin(){
+   document.getElementById('msgBox').parentElement.className = "";
+   if(document.getElementById('login').className == "on"){
+     document.getElementById('login').className = "";
+     document.getElementById('register').className = "on";
+     document.getElementById('toggleBtn').innerText = "deja un compte ? (cliquez ici)";
+   }else{
+     document.getElementById('login').className = "on";
+     document.getElementById('register').className = "";
+     document.getElementById('toggleBtn').innerText = "pas de compte ? (cliquez ici)";
+   }
+ }
+
+ function onLoginSubmit(){
+   document.getElementById('msgBox').parentElement.className ="on";
+   if(document.getElementById('login').className == "on"){
+     //ajax to log
+     //if answer true
+     //we reload and setcookie
+     //TODO window.location.reload();
+     setCookie('last','profil');
+     window.location.href = "/index_alt.html"; //TODO temp for demo
+   }
+
+
+   return false;
+ }
